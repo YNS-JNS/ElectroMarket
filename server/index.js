f7ecd102a8092850b2a94198e74e2228cef31242
yns-jns <younessaitmbarek97@gmail.com>
2024-08-16 21:13:15 +0100

    Create a user API and Add a photoUpload middleware using multer

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ import { notFound, globalErrorHandler } from "./app/middlewares/errorHandler.js"
 import productRoutes from './app/routes/product.routes.js';
 import categoryRoutes from './app/routes/category.routes.js';
 import brandRoutes from './app/routes/brand.routes.js';
+import userRoutes from './app/routes/user.routes.js';
 
 // ________________________________________________________________________
 const app = express();
@@ -36,6 +37,7 @@ connectToDB();
 app.use('/api/v1/products', productRoutes);
 app.use('/api/v1/categories', categoryRoutes);
 app.use('/api/v1/brands', brandRoutes);
+app.use('/api/v1/user', userRoutes);
 
 // Error handler middleware: ______________________________________________
 app.use(notFound);
