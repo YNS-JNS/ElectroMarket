d1abf3e2921d07bc76abb59433a32a491fff4173
yns-jns <younessaitmbarek97@gmail.com>
2024-08-27 18:19:37 +0100

    Added a login user controller

--- server/app/routes/auth.routes.js
+++ server/app/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { photoUpload } from '../middlewares/uploadImages.js';
-import { registerUserCtrl } from '../controllers/auth.controllers.js';
-import { registerUserValidator } from '../middlewares/validations/authValidator.js';
+import { registerUserCtrl, loginUserCtrl } from '../controllers/auth.controllers.js';
+import { registerUserValidator, loginUserValidator } from '../middlewares/validations/authValidator.js';
 
 const router = express.Router();
 
 router.post('/register', photoUpload.single("image"), registerUserValidator, registerUserCtrl);
+router.post('/login', loginUserValidator, loginUserCtrl);
 
 
 export default router;
