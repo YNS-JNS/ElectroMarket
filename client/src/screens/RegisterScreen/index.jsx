c8d0d3adba4f39bd0b4c73af01239eca91417458
yns-jns <younessaitmbarek97@gmail.com>
2024-08-29 17:22:18 +0100

    Created Login Screen

---
+++ client/src/screens/RegisterScreen/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RegisterScreen = () => {
+    return (
+        <div className="h-screen flex items-center justify-center p-4">
+            <div className="bg-white p-6 shadow-lg rounded-xl w-96 dark:bg-slate-100">
+              <h1>Register</h1>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterScreen;
