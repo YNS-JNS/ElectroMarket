832bbf9c1d13f1a0b7c68f876e48dca20c923384
yns-jns <younessaitmbarek97@gmail.com>
2024-09-27 18:38:33 +0100

    Enhancing Register controller

--- server/app/controllers/auth.controllers.js
+++ server/app/controllers/auth.controllers.js
@@ -2,18 +2,18 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/user.model.js';
 // import { cloudinaryUploadImage } from '../utils/cloudinary.js';
-import path from 'path';
+// import path from 'path';
 // import fs from 'fs';
-import bcrypt from 'bcryptjs';
-import { fileURLToPath } from 'url';
+// import bcrypt from 'bcryptjs';
+// import { fileURLToPath } from 'url';
 // import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import generateToken from '../utils/generateToken.js';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 /** ----------------------------------------------
 * @desc    Register New User
@@ -23,54 +23,40 @@ const __dirname = path.dirname(__filename);
 ----------------------------------------------- */
 const registerUserCtrl = asyncHandler(async (req, res) => {
 
+    const { username, email, password } = req.body;
     // 1. Validation:
-    // Check if user already exists
-    const existingUser = await User.findOne({ email: req.body.email });
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
 
-    // check if user already exists
     if (existingUser) {
-        return res.status(400).json({
-            success: false,
-            message: 'User already exists!'
-        })
+        res.status(400);
+        throw new Error('User already exists');
     }
 
-    // 4. Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-    // 5. Create and save the new user
+    // 2. Create and save the new user
     const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: hashedPassword,
-        // profilePhoto: {
-        //     url: result.secure_url,
-        //     publicId: result.public_id
-        // },
-        isAdmin: req.body.isAdmin,
-        isAccountVerified: req.body.isAccountVerified
+        username,
+        email,
+        password
     });
 
-    console.log("New User: ", newUser);
     await newUser.save();
 
-     // 3. Generate token
-     const token = generateToken(
-        {
-            sub: newUser._id,
-            // username: existingUser.username,
-            // email: existingUser.email,
-            // isAdmin: existingUser.isAdmin,
-            // isAccountVerified: existingUser.isAccountVerified
-        }
-    );
+    // 3. Generate token
+    const token = generateToken({ sub: newUser._id});
 
-    // 7. Send response to client
+    // 4. Send response to client
     res.status(201).json({
         success: true,
         message: 'User created successfully',
-        data: newUser,
+        data: {
+            _id: newUser._id,
+            username: newUser.username,
+            email: newUser.email,
+            profilePhoto: newUser.profilePhoto,
+            isAdmin: newUser.isAdmin,
+            isAccountVerified: newUser.isAccountVerified,
+          },
         token
     });
 });
