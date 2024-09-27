196218fdf047054bfa91be54d62530a054c5de26
yns-jns <younessaitmbarek97@gmail.com>
2024-09-27 18:31:50 +0100

    Updated user schema by adding pre-save hook and comparePassword method

--- server/app/controllers/auth.controllers.js
+++ server/app/controllers/auth.controllers.js
@@ -1,12 +1,12 @@
 // server\app\controllers\auth.controllers.js:
 import asyncHandler from 'express-async-handler';
 import User from '../models/user.model.js';
-import { cloudinaryUploadImage } from '../utils/cloudinary.js';
+// import { cloudinaryUploadImage } from '../utils/cloudinary.js';
 import path from 'path';
-import fs from 'fs';
+// import fs from 'fs';
 import bcrypt from 'bcryptjs';
 import { fileURLToPath } from 'url';
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import generateToken from '../utils/generateToken.js';
 
@@ -22,6 +22,61 @@ const __dirname = path.dirname(__filename);
 * @access  public
 ----------------------------------------------- */
 const registerUserCtrl = asyncHandler(async (req, res) => {
+
+    // 1. Validation:
+    // Check if user already exists
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    // check if user already exists
+    if (existingUser) {
+        return res.status(400).json({
+            success: false,
+            message: 'User already exists!'
+        })
+    }
+
+    // 4. Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // 5. Create and save the new user
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+        // profilePhoto: {
+        //     url: result.secure_url,
+        //     publicId: result.public_id
+        // },
+        isAdmin: req.body.isAdmin,
+        isAccountVerified: req.body.isAccountVerified
+    });
+
+    console.log("New User: ", newUser);
+    await newUser.save();
+
+     // 3. Generate token
+     const token = generateToken(
+        {
+            sub: newUser._id,
+            // username: existingUser.username,
+            // email: existingUser.email,
+            // isAdmin: existingUser.isAdmin,
+            // isAccountVerified: existingUser.isAccountVerified
+        }
+    );
+
+    // 7. Send response to client
+    res.status(201).json({
+        success: true,
+        message: 'User created successfully',
+        data: newUser,
+        token
+    });
+});
+
+/*
+const updateProfileUserCtrl = asyncHandler(async (req, res) => {
 
     // 1. Validation:
     // Check if user already exists
@@ -108,6 +163,7 @@ const registerUserCtrl = asyncHandler(async (req, res) => {
         data: newUser
     });
 });
+*/
 
 /** ----------------------------------------------
 * @desc    Login User
